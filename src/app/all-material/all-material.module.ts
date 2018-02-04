73cca51cb3881fb274f150c1ee3d23807d256acc
Daniel Smith <dan@callrail.com>
2018-02-03 20:08:34 -0500

    Add card list component for displaying cards

--- src/app/all-material/all-material.module.ts
+++ src/app/all-material/all-material.module.ts
@@ -8,6 +8,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   exports: [
@@ -18,7 +20,9 @@ import { MatListModule } from '@angular/material/list';
     MatSlideToggleModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule
   ]
 })
 export class AllMaterialModule { }
