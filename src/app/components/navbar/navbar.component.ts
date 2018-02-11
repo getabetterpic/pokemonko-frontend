97fd933687d1ce7ec586c16e429a05619a55fc5f
Daniel Smith <dan@callrail.com>
2018-02-11 10:56:57 -0500

    Get swipe left/right working for sidenav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+declare const Hammer: any;
+
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { MatSidenavContainer, MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'pko-navbar',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
 
   ngOnInit() {
+    const mc = new Hammer(this.element.nativeElement);
+    mc.on('swiperight', () => this.sidenav.open());
+    mc.on('swipeleft', () => this.sidenav.close());
   }
 
 }
